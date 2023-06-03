cdb6e3f49b190aed3643caacb55e038b3a8a5496
rethissunstar <rethissunstar@gmail.com>
2023-06-03 14:23:28 -0400

    got the base functionality of loading notes working

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -26,7 +26,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/api/db', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -34,7 +34,7 @@ const getNotes = () =>
   });
 
 const saveNote = (note) =>
-  fetch('/api/notes', {
+  fetch('/api/db', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ const saveNote = (note) =>
   });
 
 const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+  fetch(`/api/db:${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
---
+++ Develop/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs/promises');
+const db = require('./db/db.json');
+const PORT = 3001;
+
+
+const app = express(); 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+const PATH = require('path');
+
+
+// app.get('/', (req,res) => {
+//     res.sendFile(PATH.join(__dirname, '/db/db.json'));
+// });
+
+app.get('/', (req,res) =>{
+    res.sendFile(PATH.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(PATH.join(__dirname, '/public/notes.html'));
+})
+
+
+//this is waiting for the test note to be posted let's do that
+app.get('/api/db', (req, res) =>{
+    res.json(db);
+    console.log('this was gotten');
+})
+
+app.post('/notes', (req,res) => {
+   return res.json(db);
+   console.log(db);
+})
+
+app.listen(PORT, () =>{
+    console.log(`server listening on PORT: ${PORT}`);
+})
