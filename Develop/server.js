f8a2d1371f947b4e3072b5b097874dff473e07d5
rethissunstar <rethissunstar@gmail.com>
2023-07-13 14:14:04 -0400

    adding the ID caused stuff to work

--- Develop/server.js
+++ Develop/server.js
@@ -32,39 +32,61 @@ app.get('/api/db', (req, res) =>{
     console.log('this was gotten');
 })
 
-app.post('/api/db', (req,res) => {
-    let newnote= {
-        title: req.body.title,
-        text: req.body.text
-    }
-    console.log(newnote);
-    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    //     if (err) throw err;
+app.get('/api/db', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+// app.post('/api/db', (req,res) => {
+//     let newnote= {
+//         title: req.body.title,
+//         text: req.body.text
+//     }
+//     console.log(newnote);
+//     // fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//     //     if (err) throw err;
 
-    //         console.log('post was successful');
-    let response = `${JSON.stringify(req.body)}`;
-    console.log(response);
-    console.log(db);
-   //return req.body;
-   oldNotes = `${JSON.stringify(db[0])}`;
-   collNotes = response + ',' + oldNotes;
-   console.log(oldNotes);
-   let myArray = [collNotes];
-   console.log(myArray);
-    fs.writeFile('./db/db.json', "["+myArray+"]");
+//     //         console.log('post was successful');
+//     let response = `${JSON.stringify(req.body)}`;
+//     console.log(response);
+//     console.log(db);
+//    //return req.body;
+//    oldNotes = `${JSON.stringify(db[0])}`;
+//    collNotes = response + ',' + oldNotes;
+//    console.log(oldNotes);
+//    let myArray = [collNotes];
+//    console.log(myArray);
+//     //fs.writeFile('./db/db.json', "["+myArray+"]");
+//     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+//         if (err) throw err;
+//         res.send(db);
+//       });
+//       db.push(newnote);
+//         // const notes = JSON.parse(data);
+//         // console.log(notes);
+//         // console.log(db);
+//         // notes.push(newnote);
+//         // fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+//         //     if (err) throw err
+//         //     res.send(notes);
+//         })
+//    // })
 
-
-        // const notes = JSON.parse(data);
-        // console.log(notes);
-        // console.log(db);
-        // notes.push(newnote);
-        // fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        //     if (err) throw err
-        //     res.send(notes);
-        })
-   // })
-
-
+app.post('/api/db', (req, res) => {
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text
+    };
+  
+    db.push(newNote); // Add the new note to the db array
+  
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+      if (err) throw err;
+      res.send(db);
+    });
+  });
 
 
 
